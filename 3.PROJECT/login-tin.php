11f46812dcd75fb9570ba2b36c2eb62aa8c3c4cd
huyhaday4 <tintran2211@gmail.com>
2020-10-12 10:41:42 +0700

    update

--- 3.PROJECT/login-tin.php
+++ 3.PROJECT/login-tin.php
@@ -7,11 +7,12 @@
     <form>
       <div class="form-login">
       <h5><span style="color:blue">L</span><span style="color:red">O</span><span style="color:yellow">G</span><span style="color:green">I</span><span style="color:violet">N</span></h5>
-      <div class="form-group">
-        <label for="">Username</label>
-        <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="Enter Username">
-        <small id="helpId" class="form-text text-muted"></small>
-      </div>
+      <div class="form-group" style="margin-top:25px">
+							<label for="username">Email đăng nhập</label>
+							<input class="form-control" type="text" name="username" id="username" placeholder="tài khoản đăng nhập" required="" ng-model="Username">
+							<div class="valid-feedback">Nhập thành công.</div>
+							<div class="invalid-feedback">Hãy điền đẩy đủ thông tin.</div>
+						</div>
       <div class="form-group">
         <label for="">Password</label>
         <input type="password" class="form-control" name="" id="" placeholder="Enter Password">
