0de04678bb8026bf618c6d770436e8a621be4369
Nguyên <71805130+DamNguyen26@users.noreply.github.com>
2020-10-16 23:25:52 +0700

    Update Connection.php

--- 3.PROJECT/include/Connection.php
+++ 3.PROJECT/include/Connection.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$db   = 'Project';
+$db   = 'Mid-Test';
 $conn =  mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
     die('Khong the ket noi vi ..');
