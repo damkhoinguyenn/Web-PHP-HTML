cb30953d0050e2f3c120b0e8d1956759b84c11ae
huyhaday4 <tintran2211@gmail.com>
2020-11-12 17:50:02 +0700

    update

--- PROJECT/admin(old)/delete.php
+++ PROJECT/admin(old)/delete.php
@@ -5,40 +5,17 @@ if (!isset($_SESSION['Email'])) {
     exit();
 }
 ?>
-<!doctype html>
-<html lang="en">
 
-<head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+$user_id = $_GET['id'];
+require("../include/func/connection.php");
+include("../include/func/functions.php");
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-
-<main class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <form action="operation/delete.php" method="post">
-                <div class="form-group">
-                    <label for="txtFullName">Full Name</label>
-                    <input type="text" name="txtFullName" id="txtFullName" class="form-control" aria-describedby="helpId" value="<?php echo $user['FullName']; ?>">
-                </div>
-
-                <div class="form-group">
-                    <label for="txtEmail">Email</label>
-                    <input type="email" class="form-control" name="txtEmail" id="txtEmail" aria-describedby="emailHelpId" value="<?php echo $user['Email']; ?>">
-                </div>
-
-                <div class=" form-group">
-                    <button type="back" class="bg-light " name="btnCancel" id="btnCancel">
-                        <a class="btnCancel" href="index.php">Cancel</a>
-                    </button>
-                </div>
-
-            </form>
-        </div>
-    </div>
-</main>
+if (deleteUser($user_id)) {
+    header("Location:index.php");
+    exit();
+} else {
+    echo "Xay ra loi....";
+}
+mysqli_close($conn);
+?>
--- PROJECT/user/GVpage.php
+++ PROJECT/user/GVpage.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
-if (!isset($_SESSION['Email'])) {
-    header("Location: loginteacher.php");
-    exit();
-}
+// session_start();
+// if (!isset($_SESSION['Email'])) {
+//     header("Location: loginteacher.php");
+//     exit();
+// }
 ?>
 <!-- headerlogo -->
 <div class="container hdl">
@@ -106,7 +106,7 @@ if (!isset($_SESSION['Email'])) {
 			<div class="card " style="height:120px;">
 				<div class="row no-gutters">
 					<div class="col-md-4">
-						<img width="10" height="100" src="../images/chaok62.jpg" class="card-img" alt="...">
+						<img width="10" height="100" src="../images/dai-hoc-thuy-loi.jpg" class="card-img" alt="...">
 					</div>
 					<div class="col-md-8">
 						<div class="card-body">
@@ -118,7 +118,7 @@ if (!isset($_SESSION['Email'])) {
 			<div class="card" style="height:120px;">
 				<div class="row no-gutters">
 					<div class="col-md-4">
-						<img width="10" height="100" src="../images/chaok62.jpg" class="card-img" alt="...">
+						<img width="10" height="100" src="../images/inra1.jpg" class="card-img" alt="...">
 					</div>
 					<div class="col-md-8">
 						<div class="card-body">
@@ -130,7 +130,7 @@ if (!isset($_SESSION['Email'])) {
 			<div class="card" style="height:120px;">
 				<div class="row no-gutters">
 					<div class="col-md-4">
-						<img width="10" height="100" src="../images/chaok62.jpg" class="card-img" alt="...">
+						<img width="10" height="100" src="../images/cse-tlu-narathumb.jpg" class="card-img" alt="...">
 					</div>
 					<div class="col-md-8">
 						<div class="card-body">
@@ -251,32 +251,29 @@ if (!isset($_SESSION['Email'])) {
 		<div class="col-md-12">
 			<div class="row no-gutters bg-light position-relative">
 				<div class="col-md-6 mb-md-0 p-md-4">
-					<img src="..." class="w-100" alt="...">
+					<img width="150" height="200" src="../images/hop.jpg" class="card-img" alt="...">
 				</div>
 				<div class="col-md-6 position-static p-4 pl-md-0">
-					<h5 class="mt-0">Columns with stretched link</h5>
-					<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-					<a href="#" class="stretched-link">Go somewhere</a>
+					<p>Hội thảo Nghiên cứu ứng dụng và phát triển công nghệ khai thác nước thấm lọc từ sông phục vụ sinh hoạt và sản xuất.</p>
+					<a href="#" class="stretched-link"></a>
 				</div>
 			</div>
 			<div class="row no-gutters bg-light position-relative">
 				<div class="col-md-6 mb-md-0 p-md-4">
-					<img src="..." class="w-100" alt="...">
+					<img width="150" height="200" src="../images/thongbao.jpg" class="card-img" alt="...">
 				</div>
 				<div class="col-md-6 position-static p-4 pl-md-0">
-					<h5 class="mt-0">Columns with stretched link</h5>
-					<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-					<a href="#" class="stretched-link">Go somewhere</a>
+					<p>Thông báo về: Chương trình Hội nghị khoa học thường niên năm 2020 và Giấy mời tham dự hội nghị</p>
+					<a href="#" class="stretched-link"></a>
 				</div>
 			</div>
 			<div class="row no-gutters bg-light position-relative">
 				<div class="col-md-6 mb-md-0 p-md-4">
-					<img src="..." class="w-100" alt="...">
+					<img width="150" height="200" src="../images/1.jpg" class="card-img" alt="...">
 				</div>
 				<div class="col-md-6 position-static p-4 pl-md-0">
-					<h5 class="mt-0">Columns with stretched link</h5>
-					<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-					<a href="#" class="stretched-link">Go somewhere</a>
+					<p>Hội thảo tham vấn lấy ý kiến chuyên gia về các giải pháp chỉnh trị, chống sa bồi cửa Tiên Châu và khu neo đậu tàu thuyền Lạch Vạn Củi, tỉnh Phú Yên.</p>
+					<a href="#" class="stretched-link"></a>
 				</div>
 			</div>
 		</div>
