55d977d1004961a15951d6e9f01ac691778e0825
huyhaday4 <tintran2211@gmail.com>
2020-11-12 16:39:49 +0700

    update

--- PROJECT/user/GVpage.php
+++ PROJECT/user/GVpage.php
@@ -251,32 +251,29 @@
 		<div class="col-md-12">
 			<div class="row no-gutters bg-light position-relative">
 				<div class="col-md-6 mb-md-0 p-md-4">
-					<img src="..." class="w-100" alt="...">
+					<img width="150" height="200" src="../images/hop.jpg" class="card-img" alt="...">
 				</div>
 				<div class="col-md-6 position-static p-4 pl-md-0">
-					<h5 class="mt-0">Columns with stretched link</h5>
-					<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-					<a href="#" class="stretched-link">Go somewhere</a>
+					<p>Hội thảo Nghiên cứu ứng dụng và phát triển công nghệ khai thác nước thấm lọc từ sông phục vụ sinh hoạt và sản xuất.</p>
+					<a href="#" class="stretched-link"></a>
 				</div>
 			</div>
 			<div class="row no-gutters bg-light position-relative">
 				<div class="col-md-6 mb-md-0 p-md-4">
-					<img src="..." class="w-100" alt="...">
+					<img width="150" height="200" src="../images/thongbao.jpg" class="card-img" alt="...">
 				</div>
 				<div class="col-md-6 position-static p-4 pl-md-0">
-					<h5 class="mt-0">Columns with stretched link</h5>
-					<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-					<a href="#" class="stretched-link">Go somewhere</a>
+					<p>Thông báo về: Chương trình Hội nghị khoa học thường niên năm 2020 và Giấy mời tham dự hội nghị</p>
+					<a href="#" class="stretched-link"></a>
 				</div>
 			</div>
 			<div class="row no-gutters bg-light position-relative">
 				<div class="col-md-6 mb-md-0 p-md-4">
-					<img src="..." class="w-100" alt="...">
+					<img width="150" height="200" src="../images/1.jpg" class="card-img" alt="...">
 				</div>
 				<div class="col-md-6 position-static p-4 pl-md-0">
-					<h5 class="mt-0">Columns with stretched link</h5>
-					<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-					<a href="#" class="stretched-link">Go somewhere</a>
+					<p>Hội thảo tham vấn lấy ý kiến chuyên gia về các giải pháp chỉnh trị, chống sa bồi cửa Tiên Châu và khu neo đậu tàu thuyền Lạch Vạn Củi, tỉnh Phú Yên.</p>
+					<a href="#" class="stretched-link"></a>
 				</div>
 			</div>
 		</div>
